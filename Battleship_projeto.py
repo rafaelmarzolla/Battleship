cf9dab9b1fac130d27127f7581e8c495cd027a16
rafaelmarzolla <rafealmarzolla@yahoo.com.br>
2015-10-10 17:26:44 -0300

    Inicio do projeto

---
+++ Battleship_projeto.py
@@ -0,0 +1,36 @@
+
+
+
+#dimenções dos navios
+destroyer = 3
+fragata = 4
+porta_avioes = 5
+submarino = 2
+
+campo = []	
+for i in range (6): #loop que cria o campo para a batalha
+	
+	w = [0]*6
+	campo.append(w)
+
+#O computador vai criar campos aleatorios de combate, ele irá randomizar uma série de campos previamente criados
+
+
+def campo_1(c): #função para a primeira opção de campo
+	campo[i] = destroyer
+	return campo_1
+print(campo)
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+	
+
+
+		
